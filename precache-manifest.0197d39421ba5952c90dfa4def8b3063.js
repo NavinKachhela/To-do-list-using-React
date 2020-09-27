805ba630772ce29fd27b6fbb74550ec8a68d464b
Navin H Kachhela <kachhelanavin@gmail.com>
2020-09-28 00:57:58 +0530

    Updates

---
+++ precache-manifest.0197d39421ba5952c90dfa4def8b3063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a30f133b6480d2c25a3f06a1272c72",
+    "url": "/To-do-list-using-React/index.html"
+  },
+  {
+    "revision": "51fff04877220c7a4006",
+    "url": "/To-do-list-using-React/static/css/main.3fece945.chunk.css"
+  },
+  {
+    "revision": "b5be1c7acdc599df1bf7",
+    "url": "/To-do-list-using-React/static/js/2.8141c7e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list-using-React/static/js/2.8141c7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51fff04877220c7a4006",
+    "url": "/To-do-list-using-React/static/js/main.2fcab49b.chunk.js"
+  },
+  {
+    "revision": "50365064363480bd8c4c",
+    "url": "/To-do-list-using-React/static/js/runtime-main.fba23409.js"
+  }
+]);
